e40cb58db1a434c8900127e5f515215b0a19cab4
Alexandru Rusovici <alex.rusovici@yahoo.com>
2022-11-06 13:57:46 +0200

    First commit - Button Styleguide
    First commit - Button Styleguide

---
+++ pages/index.jsx
@@ -0,0 +1,19 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>hellow world</h1>
+
+      <h2>this is just a test</h2>
+
+      <img src="/images/banners/logo_banner.png" alt="Logo Banner" />
+
+      <a className="button button-primary" href="/">View all</a>
+      <a className="button button-primary button-primary-large" href="/">View all</a>
+
+      <a className="button button-secondary" href="/">New in</a>
+
+      <a className="button button-primary button-primary-inverted" href="/">Submit</a>
+
+    </div>
+  );
+}
